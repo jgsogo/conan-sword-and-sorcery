fae312a9d4010f8a18dd71a02b0020a2defde6de
Javier G. Sogo <jgsogo@gmail.com>
2018-08-01 12:50:58 +0200

    simple function to count registered compilers

--- tests/utils.py
+++ tests/utils.py
@@ -12,7 +12,25 @@ class TestCaseEnvClean(unittest.TestCase):
             super(TestCaseEnvClean, self).run(*args, **kwargs)
 
 
-def count_registered_compilers():
+def count_registered_compilers(id=None, osys=None, **kwargs):
+    id = id if isinstance(id, list) or id is None else [id, ]
+    osys = osys if isinstance(osys, list) or osys is None else [osys, ]
+    compiler_kwargs = {}
+    for k, v in kwargs.items():
+        value = v if isinstance(v, list) or v is None else [v, ]
+        compiler_kwargs[k] = value
+
+    n = 0
     for class_holder in CompilerRegistry._registry:
-        for conf in class_holder.explode():
-            print(conf._data)
+        if id and class_holder.compiler_class.id not in id:
+            continue
+        if osys and class_holder.compiler_class.osys not in osys:
+            continue
+
+        for compiler_instance in class_holder.explode():
+            for key, value in compiler_kwargs.items():
+                compiler_key_value = getattr(compiler_instance, key, None)
+                if compiler_key_value and compiler_key_value not in value:
+                    continue
+            n += 1
+    return n
