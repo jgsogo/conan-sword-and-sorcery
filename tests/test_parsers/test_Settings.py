01033aa4d7551662758a34055f462ce88c06cb5f
Javier G. Sogo <jgsogo@gmail.com>
2018-08-07 18:14:55 +0200

    add test settings from file

--- tests/test_parsers/test_Settings.py
+++ tests/test_parsers/test_Settings.py
@@ -2,8 +2,11 @@
 
 import os
 import unittest
+import tempfile
 
 from conans.model.settings import Settings
+from conans.client.conf import default_settings_yml
+
 from conan_sword_and_sorcery.parsers.settings import get_settings
 
 
@@ -17,4 +20,12 @@ class TestSettings(unittest.TestCase):
         with self.assertRaises(IOError):
             get_settings(filename='invalid_filename.txt')
 
-
+    def test_settings_from_filename(self):
+        tf = tempfile.NamedTemporaryFile(mode='w', delete=False)
+        tf.write(default_settings_yml)
+        tf.close()
+        try:
+            settings = get_settings(filename=tf.name)
+            self.assertIsInstance(settings, Settings)
+        finally:
+            os.remove(tf.name)
