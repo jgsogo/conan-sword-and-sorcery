bdece0d36842307c26c8866d225b7d144b4ec958
Javier G. Sogo <jgsogo@gmail.com>
2018-08-15 10:20:05 +0200

    no compiler will never have environment filters, so let the parent class implementation raise is called

--- conan_sword_and_sorcery/ci/compilers/no_compiler.py
+++ conan_sword_and_sorcery/ci/compilers/no_compiler.py
@@ -7,7 +7,3 @@ from conan_sword_and_sorcery.ci.compilers.base_compiler import BaseCompiler
 class NoCompiler(BaseCompiler):  # TODO: Refactor to 'HeaderOnlyCompiler'
     id = 'no-compiler'
 
-    @classmethod
-    def environment_filters(cls):
-        return {}
-
